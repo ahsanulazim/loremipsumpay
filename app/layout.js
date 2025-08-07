c5c2db128279a9885fc21700f079c42669c27230
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-08-08 03:20:33 +0600

    font fixing 3

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,24 @@
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
+import localFont from 'next/font/local'
+
+const helveticaNowDisplay = localFont({
+  src: [
+    {
+      path: './fonts/HelveticaNowDisplay-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+
+    {
+      path: './fonts/HelveticaNowDisplay-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+
+  ],
+})
 
 export const metadata = {
   title: "Lorem Ipsum",
@@ -14,7 +31,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={helveticaNowDisplay.className}>
         <Navbar />
         {children}
         <Footer />
